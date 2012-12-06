365cce97f2e95034a52c56b2baa127f1db364cff
Nahuel Barrios <barrios.nahuel@gmail.com>
2012-12-06 14:11:12 -0300

    Add local.properties and add it to .gitignore file.

--- gen/org/nbempire/android/BuildConfig.java
+++ gen/org/nbempire/android/BuildConfig.java
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2012 Nahuel Barrios <barrios.nahuel@gmail.com>.
- * No se reconocerá ningún tipo de garantía.
- */
-
 /** Automatically generated file. DO NOT MODIFY */
 package org.nbempire.android;
 
