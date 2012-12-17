36bbfd6d62edc422b6665ba7cd683ca25e58987e
Nahuel Barrios <barrios.nahuel@gmail.com>
2012-12-17 18:46:44 -0300

    Update Copyright adding 2013 year.

--- gen/org/nbempire/android/tourguide/Manifest.java
+++ gen/org/nbempire/android/tourguide/Manifest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2012-2013 Nahuel Barrios <barrios.nahuel@gmail.com>.
+ * No se reconocerá ningún tipo de garantía.
+ */
+
 package org.nbempire.android.tourguide;
 
 /* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
